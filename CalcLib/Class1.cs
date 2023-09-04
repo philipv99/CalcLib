6bb4a33639e4b0606572b83f67872b91d4b064f7
philipv99 <philipv9913@gmail.com>
2023-09-04 10:36:02 +0200

    Add project files.

---
+++ CalcLib/Class1.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Numerics;
+
+public class Claculator
+{
+    /// <summary>
+    /// adds to numbers, and returns a double
+    /// </summary>
+    /// <param name="n1"></param>
+    /// <param name="n2"></param>
+    /// <returns></returns>
+    public static double Plus(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+
+    public static double Plus(int n1, int n2)
+    {
+        Convert.ToDouble(n1);
+        Convert.ToDouble(n2);
+
+        return n1 + n2;
+    }
+
+    public static double Minus(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    public static double Multiply(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+    public static double Devid(double n1, double n2)
+    {
+        return n1 / n2;
+    }
+
+    /// <summary>
+    /// calculate acording to char:ops (opetator) (+ , - , * , /)
+    /// </summary>
+    /// <param name="n1"></param>
+    /// <param name="n2"></param>
+    /// <param name="ops"></param>
+    /// <returns>double</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static double Calc(double n1, double n2, char ops )
+    {
+        if (ops == null) { throw new ArgumentNullException(nameof(ops)); }
+        switch (ops) 
+        {
+            case '+': return Plus(n1, n2);
+            case '-': return Minus(n1, n2);
+            case '*': return Multiply(n1, n2);
+            case '/': return Devid(n1, n2);
+            default: throw new ArgumentException("charrector not acceptet");
+        }
+    }
+}
---
+++ CalcLibTests/Class1Tests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void PlusTest()
+        {
+            var number = Claculator.Plus(2,2);
+            Assert.AreEqual(4, number);
+        }
+
+        [TestMethod()]
+        public void PlusTest1()
+        {
+            var number = Claculator.Plus(2.0, 2.5);
+            Assert.AreEqual(4.5, number);
+        }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            var number = Claculator.Minus(10, 5.5);
+            Assert.AreEqual(4.5, number);
+        }
+
+        [TestMethod()]
+        public void MinusTestUnderZero()
+        {
+            var number = Claculator.Minus(5, 10);
+            Assert.AreEqual(-5, number);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var number = Claculator.Multiply(2, 5);
+            Assert.AreEqual(number, 10);
+        }
+
+        [TestMethod()]
+        public void DevidTest()
+        {
+            var number = Claculator.Devid(3, 3);
+            Assert.AreEqual(1, number);
+        }
+
+        [TestMethod()]
+        public void CalcTestplus()
+        {
+            var number = Claculator.Calc(2, 2, '+');
+            Assert.AreEqual(4, number);
+        }
+        [TestMethod()]
+        public void CalcTestminus()
+        {
+            var number = Claculator.Calc(2, 2, '-');
+            Assert.AreEqual(0, number);
+        }
+        public void CalcTestmulty()
+        {
+            var number = Claculator.Calc(5, 2, '*');
+            Assert.AreEqual(10, number);
+        }
+        public void CalcTestdevid()
+        {
+            var number = Claculator.Calc(4, 4, '/');
+            Assert.AreEqual(1, number);
+        }
+    }
+}
